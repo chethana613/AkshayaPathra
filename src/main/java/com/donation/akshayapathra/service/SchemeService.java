02fbe287b7670d637c58e5fabd5e291be7627183
User1 <User1@LP-5CD9296CLJ.HCLT.CORP.HCL.IN>
2020-02-14 17:49:23 +0530

    view overall schemes-piechart implementation

--- src/main/java/com/donation/akshayapathra/service/SchemeService.java
+++ src/main/java/com/donation/akshayapathra/service/SchemeService.java
@@ -2,6 +2,7 @@ package com.donation.akshayapathra.service;
 
 import java.util.List;
 
+import com.donation.akshayapathra.dto.AnalysisResponseDto;
 import com.donation.akshayapathra.dto.UserSchemeDto;
 import com.donation.akshayapathra.entity.Scheme;
 import com.donation.akshayapathra.exception.SchemeNotFoundException;
@@ -11,5 +12,7 @@ public interface SchemeService {
 	List<UserSchemeDto> getUserSchemes(Integer schemeId) throws SchemeNotFoundException;
 
 	public List<Scheme> viewAllDonations() throws SchemeNotFoundException;
+	
+	public List<AnalysisResponseDto> getAnalysis();
 
 }
